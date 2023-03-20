393a87474f37b4423778f0a1c8fc144e5ae386a4
Roland Kuhn <rk@rkuhn.info>
2023-03-20 14:31:43 +0100

    add StateOpaque.cast

--- machine-runner/src/runner/runner.ts
+++ machine-runner/src/runner/runner.ts
@@ -258,8 +258,11 @@ export const createMachineRunnerInternal = <Payload>(
   return self
 }
 
-export interface StateOpaque<P = unknown> extends StateRaw<string, P> {
-  is<Payload>(factory: StateFactory<any, any, any, Payload, any>): this is StateOpaque<Payload>
+export interface StateOpaque<StateName extends string = string, Payload = unknown>
+  extends StateRaw<StateName, Payload> {
+  is<Name extends string, Payload>(
+    factory: StateFactory<any, any, Name, Payload, any>,
+  ): this is StateOpaque<Name, Payload>
 
   as<
     StateName extends string,
@@ -278,10 +281,14 @@ export interface StateOpaque<P = unknown> extends StateRaw<string, P> {
     factory: StateFactory<any, any, StateName, StatePayload, Commands>,
     then: Then,
   ): ReturnType<Then> | undefined
+
+  cast<Commands extends CommandDefinerMap<any, any, Event.Any[]>>(
+    factory: StateFactory<any, any, StateName, Payload, Commands>,
+  ): State<StateName, Payload, Commands>
 }
 
 export namespace StateOpaque {
-  export const make = (internals: RunnerInternals.Any) => {
+  export const make = (internals: RunnerInternals.Any): StateOpaque => {
     // Capture state and factory at snapshot call-time
     const stateAtSnapshot = internals.current.data
     const factoryAtSnapshot = internals.current.factory as StateFactory.Any
@@ -319,9 +326,25 @@ export namespace StateOpaque {
       return undefined
     }
 
+    const cast: StateOpaque['cast'] = (factory) => ({
+      payload: stateAtSnapshot.payload,
+      type: stateAtSnapshot.type,
+      commands: convertCommandMapToCommandSignatureMap<any, unknown, Event.Any[]>(
+        factory.mechanism.commands,
+        {
+          isExpired,
+          getActualContext: () => ({
+            self: internals.current.data.payload,
+          }),
+          onReturn: (events) => internals.commandEmitFn?.(events),
+        },
+      ),
+    })
+
     return {
       is,
       as,
+      cast,
       payload: stateAtSnapshot.payload,
       type: stateAtSnapshot.type,
     }
