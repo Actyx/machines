dde071fa2e1e02bd20f21cf16fa37ca3723bda40
Roland Kuhn <rk@rkuhn.info>
2023-04-13 09:25:41 +0200

    export SwarmProtocol

--- machine-runner/src/index.ts
+++ machine-runner/src/index.ts
@@ -1,6 +1,6 @@
 export * from './runner/index.js'
 export { MachineEvent } from './design/event.js'
-export { Machine } from './design/protocol.js'
+export { Machine, SwarmProtocol } from './design/protocol.js'
 export { StateRaw } from './design/state.js'
 import { deepCopy } from './utils/object-utils.js'
 
